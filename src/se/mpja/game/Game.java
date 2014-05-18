2d7b4629d17670a8c874f71743fc4a4821fa72f9
mpja69 <mpja69@gmail.com>
2014-05-18 22:43:30 +0200

    First commit of the game

---
+++ src/se/mpja/game/Game.java
@@ -0,0 +1,54 @@
+package se.mpja.game;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class Game implements Runnable {
+	
+	public Game() {
+		frame = getJFrame();
+		canvas = getCanvas();
+		frame.add(canvas);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	private JFrame frame;
+	private Canvas canvas;
+	private Thread thread;
+
+	public static final int WIDTH = 300, HEIGHT = WIDTH / 16 * 12;
+	public static final String TITLE = "Game";
+	private static final int SCALE = 3;
+
+	public void start(){
+		thread = new Thread(this, "MainThread");
+		thread.start();
+	}
+
+	public void run() {
+		System.out.println("Running");
+	}
+	private Canvas getCanvas(){
+		Canvas c = new Canvas();
+		Dimension d = new Dimension(WIDTH * SCALE, HEIGHT * SCALE);
+		c.setMaximumSize(d);
+		c.setMinimumSize(d);
+		c.setPreferredSize(d);
+		return c;
+	}
+
+	private JFrame getJFrame(){
+		JFrame f = new JFrame();
+		f.setResizable(false);
+		f.setName(TITLE);
+		f.setTitle(TITLE);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		return f;
+	}
+
+	public static void main(String[] args) {
+		new Game().start();
+	}
+}
